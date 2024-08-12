2c6f01343ebfb15bf59c620e02a2e2968af6c9be
Guilherme Krieck <116123096+GuiKrieck@users.noreply.github.com>
2024-08-12 13:23:07 -0300

    Basic Structure
    Project basic structure, and API fetching(MockAPI.io)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: "https://66ba2c40fa763ff550fb0d03.mockapi.io",
+  },
+};
 
 export default nextConfig;
